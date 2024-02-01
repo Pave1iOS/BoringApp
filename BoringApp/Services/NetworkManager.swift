396402e1d8ed065c90f8772c53ff630f705c1dbd
Pavel Gribachev <PGribachev@yandex.ru>
2024-02-01 16:45:35 +0300

    create networkManager

---
+++ BoringApp/Services/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  BoringApp
+//
+//  Created by Pavel Gribachev on 01.02.2024.
+//
+
+import Foundation
+
+struct NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    
+}
