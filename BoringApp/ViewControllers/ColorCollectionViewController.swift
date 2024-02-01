ed4e3e221ed655e5895988b4dda51a09ef3b3a14
Pavel Gribachev <PGribachev@yandex.ru>
2024-02-01 23:46:31 +0300

    save before exit

---
+++ BoringApp/ViewControllers/ColorCollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ColorViewController.swift
+//  BoringApp
+//
+//  Created by Pavel Gribachev on 01.02.2024.
+//
+
+import UIKit
+
+class ColorCollectionViewController: UICollectionViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+}
+
+
