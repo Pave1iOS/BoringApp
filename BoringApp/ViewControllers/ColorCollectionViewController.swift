82f3288d63480ceb3e140350180af1bf88d25239
Pave1iOS <PGribachev94@yandex.ru>
2024-02-07 02:23:45 +0300

    close 2 view

--- BoringApp/ViewControllers/ColorCollectionViewController.swift
+++ BoringApp/ViewControllers/ColorCollectionViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ColorCollectionViewController: UICollectionViewController {
     
     let colors = BackgraundColors.allCases
-    var bgColor: UIColor!
+    var bgColorName: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,39 +31,25 @@ class ColorCollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        bgColor = UIColor(named: colors[indexPath.row].rawValue)
-        print(bgColor as Any)
+        bgColorName = colors[indexPath.row].rawValue
+        guard let color = bgColorName else { return }
+        
+        print(color)
+        
+        dismissTo(vcCount: 2, animated: true)
     }
+}
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+private extension UIViewController {
+    func dismissTo(vcCount count: Int, animated: Bool) {
+        var dummyVC: UIViewController? = self
+        
+        for _ in 0..<count {
+            dummyVC = dummyVC?.presentingViewController
+        }
+        
+        if count != 0 {
+            dummyVC?.dismiss(animated: animated)
+        }
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
